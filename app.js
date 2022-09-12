b86b8b238e8da7512dadd5c2b9bba74a29f25bb1
Nichromat <82540453+nichromat@users.noreply.github.com>
2022-09-12 14:35:24 +0200

    Create node from template on button click

---
+++ app.js
@@ -0,0 +1,24 @@
+// Name: Create content from template
+// Desc: Create a content from existing template
+// Input:
+// Output:
+
+function createContentFromTemplate() {
+
+    // Let variable 'content' as DOMElement with selector '#hashnode-content'
+    let content = document.querySelector('#hashnode-content');
+
+    // Let 'body' as DOMElement with value 'body'
+    let body = document.querySelector('body');
+
+    // Let 'nodeImport' as DOMElement with value 'importNode' -> content.content, true
+    let nodeImport = document.importNode(content.content, true);
+
+    // 'body' -> 'appendChild' -> nodeImport
+    body.appendChild(nodeImport)
+}
+
+
+let addContentBottom = document.querySelector('.add-content-to-bottom');
+
+addContentBottom.addEventListener('click', createContentFromTemplate);
